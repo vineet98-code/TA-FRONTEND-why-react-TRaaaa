abd690b6f049103d3f3eacd755668f2512e6d7b7
Vineet <vineet98love@gmail.com>
2022-01-14 22:11:25 +0530

    update

---
+++ block-BRaaax/design-system/src/components/buttoninfo.js
@@ -0,0 +1,11 @@
+export let buttonSizes = {
+    LARGE: 'large',
+    MEDIUM: 'medium',
+    SMALL: 'small',
+  };
+  
+  export let buttonTypes = {
+    PRIMARY: 'primary',
+    SECONDARY: 'secondary',
+    TERTIARY: 'tertiary',
+  };
