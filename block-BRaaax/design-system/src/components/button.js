70de5d0fd547adf36bc1d87a5337340a7e1f907a
Vineet <vineet98love@gmail.com>
2022-01-14 22:06:55 +0530

    update

--- block-BRaaax/design-system/src/components/button.js
+++ block-BRaaax/design-system/src/components/button.js
@@ -1,19 +1,19 @@
 import { buttonSizes, buttonTypes } from './buttonInfo';
-// function Button(props) {
-//   function getStyles() {
-//     let { type = buttonTypes.PRIMARY, size = buttonSizes.MEDIUM } = props;
-//     return `button button--${type} button--${size}`;
-//   }
-//   return (
-//     <button
-//       onClick={props.onClickHandler}
-//       className={getStyles()}
-//       disabled={props.disabled}
-//     >
-//       {props.label || 'Button'}
-//     </button>
-//   );
-// }
+function Button(props) {
+  function getStyles() {
+    let { type = buttonTypes.PRIMARY, size = buttonSizes.MEDIUM } = props;
+    return `button button--${type} button--${size}`;
+  }
+  return (
+    <button
+      onClick={props.onClickHandler}
+      className={getStyles()}
+      disabled={props.disabled}
+    >
+      {props.label || 'Button'}
+    </button>
+  );
+}
 
 function Button(props){
     function getStyles(){
