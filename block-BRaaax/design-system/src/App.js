aaa94917796d61919a40269b852019421012cb6f
Vineet <vineet98love@gmail.com>
2022-01-14 22:13:36 +0530

    update

--- block-BRaaax/design-system/src/App.js
+++ block-BRaaax/design-system/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
+import { buttonSizes, buttonTypes } from './buttonInfo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//     return (
+//       <div className="App">
+//         <Button label="Click Me!" />
+//         <Button
+//           label="Click Me!"
+//           type={buttonTypes.SECONDARY}
+//           size={buttonSizes.LARGE}
+//         />
+//         <Button
+//           label="Click Me!"
+//           type={buttonTypes.TERTIARY}
+//           size={buttonSizes.MEDIUM}
+//         />
+//         <Button onClickHandler={() => alert('You Clicked me')} />
+//         <Button disabled />
+//       </div>
+//     );
+//   }
+
+function App(){
+    return <div className="App">
+     <Button label="Click me"/>
+     <Button label="Click me" type={buttonTypes.SECONDARY} size={buttonSizes.LARGE}/>
+     <Button label="Click me" type={buttonTypes.PRIMARY} size={buttonSizes.MEDIUM}/>
+     <Button onClickHandler={()=> alert("you clicked me! ")}/>
+     <Button disabled/>
+     </div>
 }
 
 export default App;
