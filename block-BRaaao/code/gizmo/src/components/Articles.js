b5f585d4abd0ed2a259ca491220720eacbdad608
Vineet <vineet98love@gmail.com>
2022-02-18 19:07:10 +0530

    update

--- block-BRaaao/code/gizmo/src/components/Articles.js
+++ block-BRaaao/code/gizmo/src/components/Articles.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import {ArticleItems} from './ArticleItems';
+import  ArticleItems  from './ArticleItems'
+import  allArticles from '../data'
 
-export const Articles = (props) => {
-    return (
-        <div className = 'container flex'>
-            <h3 className="text-center my-3">Articles lists</h3>
-            {props.articles.map((article) => {
-                return <ArticleItems article={article}/>
+
+const Articles = () => {
+  return (
+    <div className = 'd-flex justify-content-evenly flex-wrap my-4'>
+            {/* <h3 className="text-center my-3">Articles lists</h3> */}
+            {allArticles.map((article) => {
+                return <ArticleItems key={article.url} {...article} />
             })}          
         </div>
-    )
+  )
 }
+
+export default Articles
--- block-BRaaao/code/gizmo/src/components/Header.js
+++ block-BRaaao/code/gizmo/src/components/Header.js
@@ -1,26 +1,14 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
 
-export default function Header() {
-    return (
-        <ul class="nav nav-pills nav-fill">
-            <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/">Home</Link>
-            </li>
-            <li class="nav-item">
-                <Link class="nav-link " to="/about">About</Link>
-            </li>
-            <li class="nav-item">
-                <Link class="nav-link" to="#">Link</Link>
-            </li>
-            <li class="nav-item">
-                <Link class="nav-link ">Contacts</Link>
-            </li>
-        </ul>
-    )
+const Header = () => {
+  return (
+    <nav class="nav nav-pills nav-fill">
+      <a class="nav-link " aria-current="page" href="/">Home</a>
+      <a class="nav-link" href="/">About</a>
+      <a class="nav-link" href="/">Contact</a>
+      
+    </nav>
+  )
 }
-// type passing as a string will throw an error, when we pass a number it will be converted to a string
-Header.propTypes = {
-    title: PropTypes.string
-}
+
+export default Header
